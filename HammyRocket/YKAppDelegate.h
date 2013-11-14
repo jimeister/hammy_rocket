17dfa7b6f916d4644d2f085e9fdaa731d84532da
Allen Cheung <acheung@yelp.com>
2013-11-14 10:52:42 -0800

    Initial Commit

---
+++ HammyRocket/YKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YKAppDelegate.h
+//  HammyRocket
+//
+//  Created by Allen Cheung on 11/14/13.
+//  Copyright (c) 2013 HammyRocket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HammyRocket/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HammyRocket
+//
+//  Created by Allen Cheung on 11/14/13.
+//  Copyright (c) 2013 HammyRocket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([YKAppDelegate class]));
+  }
+}
